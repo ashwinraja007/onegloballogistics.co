ae4d1d224539ce7a73a90199d99ed7ef1499f63b
Karthikentrepreneur <karthikjungleemara@gmail.com>
2025-10-27 21:59:54 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,6 +4,7 @@ import { Footer } from "@/components/layout/Footer";
 import { Loader2 } from "lucide-react";
 import Hero from "@/components/home/Hero";
 import { VisionMission } from "@/components/about/VisionMission";
+import { WhyChooseUs } from "@/components/about/WhyChooseUs";
 // Lazy load other components
 const AboutUs = lazy(() => import("@/components/home/AboutUs"));
 const Services = lazy(() => import("@/components/home/Services"));
