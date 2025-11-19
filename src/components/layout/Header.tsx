fb62e7212894534551f3fa31d5e6d73cf4b0031c
Karthikentrepreneur <karthikjungleemara@gmail.com>
2025-11-19 14:24:32 +0530

    Update Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -70,7 +70,7 @@ export const Header = () => {
                 className="
                   h-8          /* SMALL SCREEN ONLY SMALLER */
                   md:h-10
-                  lg:h-10
+                  lg:h-8
                   xl:h-[40px]
                   object-contain
                 "
