9bbad909aa886eb7b661263a30beede55d07664f
Karthikentrepreneur <karthikjungleemara@gmail.com>
2026-01-09 16:06:52 +0530

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -24,8 +24,8 @@ const countries = [{
     contacts: ["Export customer service: +91 2245174102, +91 2245174109, +91 2245174118", "Export docs team: +91 2245174103, +91 2245174105", "Import docs team: +91 2245174104, +91 2245174106", "Import customer service: +91 2245174113, +91 2245174110, +91 2245174107, +91 2245174112"]
   }, {
     name: "Delhi",
-    lat: 25.6083,
-    lng: 85.1437,
+    lat: 28.53837, 
+    lng: 77.29022,
     address: "JA 511, DLF Tower A, Jasola District Centre, New Delhi, 110025",
     contacts: ["+91 9999022030"]
   }, {
